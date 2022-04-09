91af9759a13134aff2599a9ffaded05836672281
Adroaldo <hygino@msn.com>
2022-04-09 09:39:56 -0300

    Classe MessageBox

---
+++ ClassesImportantes/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassesImportantes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMessageBox_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("oi piazada");
+            //MessageBox.Show("Minha menssagem", "Titulo da menssagem");
+            //MessageBox.Show("Texto da mensagem","Título",MessageBoxButtons.YesNo);
+            //MessageBox.Show("Texto da mensagem", "Título", MessageBoxButtons.YesNoCancel);
+            /*var res = MessageBox.Show("Texto da mensagem", "Título", MessageBoxButtons.OKCancel);
+            if (res == DialogResult.OK)
+            {
+                lblResultado.Text = "Clicou em OK";
+            }
+            else if (res == DialogResult.Cancel)
+            {
+                lblResultado.Text = "Clicou em Cancelar";
+            }*/
+            //MessageBox.Show("Mensagem", "Titulo", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+
+            MessageBox.Show("Mensagem", "Titulo", MessageBoxButtons.YesNo, MessageBoxIcon.Error, MessageBoxDefaultButton.Button2);
+        }
+    }
+}
