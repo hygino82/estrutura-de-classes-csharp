7bebd6e084a9ab6a640b8fa266487144ca1d3a0f
Adroaldo <hygino@msn.com>
2022-04-28 10:20:00 -0300

    Thread - classe de tarefas

---
+++ ThreadConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(Tarefa));
+            t.IsBackground = true;//finaliza as thread secundárias
+            t.Start();
+            t.Join();//tarefa secundária termina antes de começar a primária
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Principal #" + (i + 1));
+                Thread.Sleep(500);//pausa em ms
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Tarefa()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Tarefa Executada #" + (i + 1));
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
