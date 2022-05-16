e49f8fcb911fda1d880e09a454639650aed1b682
Adroaldo <hygino@msn.com>
2022-05-16 15:16:07 -0300

    Task - parte 3

--- TaskConsoles/Program.cs
+++ TaskConsoles/Program.cs
@@ -41,6 +41,7 @@ namespace TaskConsoles
             //}
             #endregion
 
+            #region Task parte 2
             //Task[] tasks =
             //{
             //    Task.Factory.StartNew(()=>{
@@ -61,27 +62,57 @@ namespace TaskConsoles
 
             //Task.WaitAll(tasks);//´precisa terminar as tarefas para seguir a execução do programa
 
-            Task t1 = Task.Run(() =>
+            //Task t1 = Task.Run(() =>
+            //{
+            //    Console.WriteLine("Comando #1");
+            //});
+
+            //Task t2 = Task.Run(() =>
+            //{
+            //    Console.WriteLine("Comando #2");
+            //});
+
+            //Task t3 = Task.Run(() =>
+            //{
+            //    Console.WriteLine("Comando #3");
+            //});
+
+
+            //Task.WaitAll(t1, t2, t3);
+            //Console.WriteLine("Tarefa Principal");
+            //Console.ReadKey();
+
+            #endregion
+
+            //Task<int> tarefa1 = Task.Factory.StartNew(() => Dobro(5));
+
+            //Console.WriteLine(tarefa1.Result);//mostra o valor obtido na tarefa
+
+            Task<int> tarefa1 = Task.Factory.StartNew(() =>
             {
-                Console.WriteLine("Comando #1");
+                return 5;//new Random().Next(10);
             });
 
-            Task t2 = Task.Run(() =>
+            Task<int> tarefa2 = tarefa1.ContinueWith((num) =>
             {
-                Console.WriteLine("Comando #2");
+                return Dobro(num.Result);
             });
 
-            Task t3 = Task.Run(() =>
+            Task<String> tarefa3 = tarefa2.ContinueWith((num) =>
             {
-                Console.WriteLine("Comando #3");
+                return "Valor final " + num.Result;
             });
 
+            Console.WriteLine(tarefa3.Result);
 
-            Task.WaitAll(t1, t2, t3);
-            Console.WriteLine("Tarefa Principal");
             Console.ReadKey();
-
         }
+
+        static int Dobro(int x)
+        {
+            return 2 * x;
+        }
+
         static void Tarefa()
         {
             Console.WriteLine("Tarefa #4");
